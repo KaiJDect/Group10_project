f72c54b8d0598a646d2d343343954c88d6b634ef
Geoffery <geoffery00@gmail.com>
2024-10-03 03:30:30 -0700

    moved correlation for lagging variables to feature engineering section

---
+++ src/notebooks/misc.py
@@ -0,0 +1,35 @@
+temperatureLagSteps = [1,13,30]
+rainLagSteps = [15, 24]
+evapLagSteps = [7, 15, 28]
+radnLagSteps = [5, 16, 28]
+vpLagSteps = [1, 10,30]
+rhLagSteps = [15, 26]
+
+
+
+# TODO:
+# cyclical transformations for datetime
+# data versioning
+# training with rollingWindowDataSets
+# fix time axis labelling
+# perform training for both nsw and qld
+# model post tuning analysis
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
